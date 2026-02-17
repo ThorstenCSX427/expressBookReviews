ec2a310b1229bdc6ce5a811a64c8e57d4f6ecde1
Thorsten Wessling <thorsten.wessling@siemens.com>
2026-02-17 17:15:18 +0100

    feat: code changes for task 3
    Signed-off-by: Thorsten Wessling <thorsten.wessling@siemens.com>

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -46,7 +46,13 @@ public_users.get('/isbn/:isbn',function (req, res) {
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author;
+  const booksByAuthor = Object.values(books).filter(book => book.author === author);
+  if (booksByAuthor.length > 0) {
+    res.send(JSON.stringify(booksByAuthor, null, 4));
+  } else {
+    res.status(404).json({message: "No books found by this author"});
+  }
 });
 
 // Get all books based on title
